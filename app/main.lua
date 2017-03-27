1ce701fdeb2776727dbd5618ed2d9f66606e3942
午夜 <sumory.wu@gmail.com>
2017-03-27 14:38:36 +0800

    make it compatible with lor v0.3

--- app/main.lua
+++ app/main.lua
@@ -18,11 +18,11 @@ app:conf("view engine", view_config.engine)
 app:conf("view ext", view_config.ext)
 app:conf("views", view_config.views)
 
-app:use(reponse_time_middleware({
-    digits = 0,
-    header = 'X-Response-Time',
-    suffix = true
-}))
+-- app:use(reponse_time_middleware({
+--     digits = 0,
+--     header = 'X-Response-Time',
+--     suffix = true
+-- }))
 
 app:use(session_middleware({
     secret = config.session_secret -- 必须修改此值
@@ -40,32 +40,30 @@ app:use(uploader_middleware({
 
 router(app) -- business routers and routes
 
--- 404 error
-app:use(function(req, res, next)
-    if req:is_found() ~= true then
-    	if sfind(req.headers["Accept"], "application/json") then
-    		res:status(404):json({
-    			success = false,
-    			msg = "404! sorry, not found."
-    		})
-    	else
-        	res:status(404):send("404! sorry, not found.")
-        end
-    end
-end)
-
 -- error handle middleware
 app:erroruse(function(err, req, res, next)
 	ngx.log(ngx.ERR, err)
-
-	if sfind(req.headers["Accept"], "application/json") then
-		res:status(500):json({
-			success = false,
-			msg = "500! unknown error."
-		})
+	
+	-- 404 error
+	if req:is_found() ~= true then
+		if sfind(req.headers["Accept"], "application/json") then
+			res:status(404):json({
+				success = false,
+				msg = "404! sorry, not found."
+			})
+		else
+			res:status(404):send("404! sorry, not found.")
+		end
 	else
-    	res:status(500):send("unknown error")
-    end
+		if sfind(req.headers["Accept"], "application/json") then
+			res:status(500):json({
+				success = false,
+				msg = "500! server error."
+			})
+		else
+			res:status(500):send("server error")
+		end
+	end
 end)
 
 app:run()
